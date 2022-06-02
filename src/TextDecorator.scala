14d584dfcc369abca3434438def054776721e5aa
Silvio Mayolo <mercerenies@comcast.net>
2022-06-01 22:25:33 -0400

    TextDecorator.scala

---
+++ src/TextDecorator.scala
@@ -0,0 +1,28 @@
+
+package com.mercerenies.werewolf
+
+import org.javacord.api.entity.message.MessageDecoration
+
+// Helpers for decorating text in preparation to be interpreted as
+// Markdown.
+case class TextDecorator(val prefix: String, val suffix: String) {
+
+  def apply(x: String): String =
+    prefix + x + suffix
+
+}
+
+object TextDecorator {
+
+  def fromMessageDecoration(decorator: MessageDecoration): TextDecorator =
+    TextDecorator(decorator.getPrefix, decorator.getSuffix)
+
+  val bold = fromMessageDecoration(MessageDecoration.BOLD)
+  val longCode = fromMessageDecoration(MessageDecoration.CODE_LONG)
+  val code = fromMessageDecoration(MessageDecoration.CODE_SIMPLE)
+  val italic = fromMessageDecoration(MessageDecoration.ITALICS)
+  val spoiler = fromMessageDecoration(MessageDecoration.SPOILER)
+  val strikeout = fromMessageDecoration(MessageDecoration.STRIKEOUT)
+  val underline = fromMessageDecoration(MessageDecoration.UNDERLINE)
+
+}
