13bb2a30a3e7ef53148b7c17a14de62aae138593
Silvio Mayolo <mercerenies@comcast.net>
2022-06-05 13:43:53 -0400

    Role parsing

--- src/state/RoleListState.scala
+++ src/state/RoleListState.scala
@@ -10,6 +10,8 @@ import logging.LogEither.*
 import name.{NameProvider, BaseNameProvider, DisplayNameProvider}
 import command.CommandResponse
 import manager.GamesManager
+import game.role.Role
+import game.parser.ListParser
 
 import org.javacord.api.DiscordApi
 import org.javacord.api.entity.channel.{TextChannel, Channel}
@@ -45,11 +47,47 @@ final class RoleListState(
     // No action
   }
 
+  private def isMessageRelevant(mgr: GamesManager, message: Message): Boolean =
+    // We only care about messages sent by the game host and which
+    // ping the bot.
+    (message.getAuthor.getId == hostId.toLong) &&
+      (util.mentions(message, Id(mgr.api.getYourself)))
+
+  // TODO This is also calculated in SignupState and should be
+  // centralized somewhere.
+  private def requiredRoleCount = players.length + 3
+
+  // TODO Default message if you ping the bot in a channel that
+  // doesn't have a game?
   override def onMessageCreate(mgr: GamesManager, message: Message): Unit = {
-    /////
+    if (isMessageRelevant(mgr, message)) {
+      val parser = RoleListState.listParser
+      parser.parse(message.getContent) match {
+        case -\/(err) => {
+          message.reply(err)
+        }
+        case \/-(roles) if roles.length != requiredRoleCount => {
+          message.reply(s"Sorry, but I'm expecting ${requiredRoleCount} role(s). You gave me a list of ${roles.length} role(s).")
+        }
+        case \/-(roles) => {
+          /////
+          println(s"I got ${roles}!")
+          message.reply("Thank you!")
+        }
+      }
+    }
   }
 
   override def onStartGame(mgr: GamesManager, interaction: SlashCommandInteraction): Future[CommandResponse[Unit]] =
     Future.successful(CommandResponse.ephemeral("This game has already started and is waiting on a role list.").void)
+
+}
+
+object RoleListState {
+
+  val validRoles: List[Role] = Role.all
+
+  private val listParser: ListParser[Role] =
+    ListParser(validRoles, "role")
 
 }
