6ad089b9be47cfba8f03d2345b03d180d40a9901
Silvio Mayolo <mercerenies@comcast.net>
2022-06-06 10:49:17 -0400

    util.findDuplicate

--- src/util/package.scala
+++ src/util/package.scala
@@ -9,6 +9,7 @@ import org.javacord.api.entity.user.User
 
 import scala.jdk.CollectionConverters.*
 import scala.util.Random
+import scala.collection.mutable.HashSet
 
 // Various convenience helpers that had nowhere better to go
 
@@ -20,4 +21,17 @@ def mentions(message: Message, userId: Id[User]): Boolean =
 def randomlyAssign[A, B](lhs: Iterable[A], rhs: Iterable[B]): Iterable[(A, B)] = {
   val shuffled = Random.shuffle(rhs)
   lhs zip shuffled
+}
+
+// Finds the first duplicated element in the iterable. If there is
+// none, then returns None.
+def findDuplicate[A](data: IterableOnce[A]): Option[A] = {
+  val collection: HashSet[A] = HashSet()
+  for (term <- data.iterator) {
+    if (collection.contains(term)) {
+      return Some(term)
+    }
+    collection += term
+  }
+  None
 }
