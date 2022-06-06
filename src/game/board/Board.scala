08066d2eea32b0560d4ba45e4ff82cdfbb7a4451
Silvio Mayolo <mercerenies@comcast.net>
2022-06-05 20:08:19 -0400

    assignRoles

--- src/game/board/Board.scala
+++ src/game/board/Board.scala
@@ -3,7 +3,10 @@ package com.mercerenies.werewolf
 package game
 package board
 
+import id.Id
 import role.{Role, RoleInstance}
+
+import org.javacord.api.entity.user.User
 
 import scala.collection.mutable.HashMap
 
@@ -17,10 +20,23 @@ final class Board private(
 
 object Board {
 
-  def apply(mapping: Map[Position, RoleInstance]) =
+  def apply(mapping: Iterable[(Position, RoleInstance)]) =
     new Board(HashMap.from(mapping))
 
-  def create(roles: Map[Position, Role]): Board =
+  def create(roles: Iterable[(Position, Role)]): Board =
     Board(roles.map { (pos, role) => (pos, role.createInstance()) })
+
+  // Precondition: players.length + Rules.centerCards == roles.length
+  def assignRoles(players: List[Id[User]], roles: List[Role]): Board = {
+    // Validate precondition
+    if (players.length + Rules.centerCards != roles.length) {
+      throw new RuntimeException(s"In a ${players.length}-player game, expected ${players.length + Rules.centerCards} roles, but got ${roles.length} instead.")
+    }
+
+    val positions = Position.forGame(players)
+    val roleAssignments = util.randomlyAssign(positions, roles)
+    Board.create(roleAssignments)
+
+  }
 
 }
