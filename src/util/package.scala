2d1ca3ccd3eb7fa22cc2a47c2860311689c9f8d4
Silvio Mayolo <mercerenies@comcast.net>
2022-06-05 20:08:14 -0400

    randomlyAssign

--- src/util/package.scala
+++ src/util/package.scala
@@ -8,8 +8,16 @@ import org.javacord.api.entity.message.Message
 import org.javacord.api.entity.user.User
 
 import scala.jdk.CollectionConverters.*
+import scala.util.Random
 
 // Various convenience helpers that had nowhere better to go
 
 def mentions(message: Message, userId: Id[User]): Boolean =
   message.getMentionedUsers.asScala.exists { _.getId == userId.toLong }
+
+// If the two lists are not of the same length, the longer one will
+// have some elements unassigned.
+def randomlyAssign[A, B](lhs: Iterable[A], rhs: Iterable[B]): Iterable[(A, B)] = {
+  val shuffled = Random.shuffle(rhs)
+  lhs zip shuffled
+}
