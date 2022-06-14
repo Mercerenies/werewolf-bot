d3d7b0c6a22ed22bb4e49fd2ef0014bf18c42275
Silvio Mayolo <mercerenies@comcast.net>
2022-06-14 11:23:57 -0400

    ArgumentMatchers in UnitSpec

--- src/test/UnitSpec.scala
+++ src/test/UnitSpec.scala
@@ -3,10 +3,11 @@ package com.mercerenies.werewolf
 
 import org.scalatest.*
 import org.scalatestplus.mockito.*
-import org.mockito.Mockito
+import org.mockito.{Mockito, ArgumentMatchers}
 import flatspec.*
 import matchers.*
 
 abstract class UnitSpec extends AnyFlatSpec with should.Matchers with MockitoSugar {
   export Mockito.*
+  export ArgumentMatchers.*
 }
