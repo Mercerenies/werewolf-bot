1a6df02a1bbf93da2f4d8d49c7a2ec0df3f3ff93
Silvio Mayolo <mercerenies@comcast.net>
2022-06-21 11:18:00 -0400

    concurrent testing framework

--- src/test/UnitSpec.scala
+++ src/test/UnitSpec.scala
@@ -4,6 +4,7 @@ package com.mercerenies.werewolf
 import id.Id
 
 import org.scalatest.*
+import org.scalatest.concurrent.*
 import org.scalatestplus.mockito.*
 import org.mockito.{Mockito, ArgumentMatchers}
 import flatspec.*
@@ -11,9 +12,10 @@ import matchers.*
 
 import org.javacord.api.entity.DiscordEntity
 
-abstract class UnitSpec extends AnyFlatSpec with should.Matchers with MockitoSugar {
+abstract class UnitSpec extends AnyFlatSpec with should.Matchers with Futures with MockitoSugar {
   export Mockito.*
   export ArgumentMatchers.*
+  export ScalaFutures.*
 
   def anyId[A <: DiscordEntity](): Id[A] =
     Id.fromLong(anyLong())
