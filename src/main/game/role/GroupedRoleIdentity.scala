3a0a171ffe487be16bd38902e252587632075136
Silvio Mayolo <mercerenies@comcast.net>
2022-06-29 14:43:24 -0400

    Some future stuff for fluffy and ripper

--- src/main/game/role/GroupedRoleIdentity.scala
+++ src/main/game/role/GroupedRoleIdentity.scala
@@ -5,7 +5,13 @@ package role
 
 // Roles which are grouped (werewolves and masons, mainly) use this to
 // identify other members of their group.
-enum GroupedRoleIdentity {
-  case Werewolf
-  case Mason
+enum GroupedRoleIdentity(val name: String) {
+  // The werewolf team, visible to each other and to the Minion.
+  case Werewolf extends GroupedRoleIdentity("Werewolf")
+  // Masons, visible to each other.
+  case Mason extends GroupedRoleIdentity("Mason")
+  // Fluffy, the target of Ripper's win condition.
+  case Fluffy extends GroupedRoleIdentity("Fluffy")
+  // Ripper, the target of Fluffy's win condition.
+  case Ripper extends GroupedRoleIdentity("Ripper")
 }
