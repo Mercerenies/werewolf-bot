7c42f17fe9bf39b591f3c1cbc9b3b630655d20b7
Silvio Mayolo <mercerenies@comcast.net>
2022-07-03 20:15:01 -0400

    copyrobber tests

--- src/test/game/role/RobberSpec.scala
+++ src/test/game/role/RobberSpec.scala
@@ -122,4 +122,66 @@ class RobberSpec extends GameplayUnitSpec {
 
   }
 
+  it should "swap roles and be given information if a copy-robber elects to act" in {
+    val board = createBoard(
+      left = Villager,
+      middle = Robber,
+      right = Villager,
+      playerCards = List(Villager, Copycat, Tanner),
+    )
+    val (finalBoard, history, feedback, _) = playGame(board, List("", ("middle", mockName(0)), ""))
+
+    finalBoard(TablePosition.Left).role should be (Villager)
+    finalBoard(TablePosition.Middle).role should be (Robber)
+    finalBoard(TablePosition.Right).role should be (Villager)
+    finalBoard(id(0)).role should be (Copycat)
+    finalBoard(id(1)).role should be (Villager)
+    finalBoard(id(2)).role should be (Tanner)
+
+    feedback(id(0)) should be (FeedbackMessage.none)
+    feedback(id(1)).mkString should include (mockName(0))
+    feedback(id(1)).mkString should include regex ("(?i)villager")
+    feedback(id(2)) should be (FeedbackMessage.none)
+
+    val filtered = filterRecords(history)
+    history.toList should have length (3)
+    filtered should have length (2)
+
+    filtered(0).displayText(SampleUserMapping(3)) should include regex ("(?i)middle")
+    filtered(0).displayText(SampleUserMapping(3)) should include regex ("(?i)robber")
+
+    filtered(1).displayText(SampleUserMapping(3)) should include (mockName(0))
+    filtered(1).displayText(SampleUserMapping(3)) should include regex ("(?i)villager")
+    filtered(1).displayText(SampleUserMapping(3)) should not include regex ("(?i)tanner|werewolf")
+
+  }
+
+  it should "act before a copy-troublemaker" in {
+    val board = createBoard(
+      left = Villager,
+      middle = Troublemaker,
+      right = Villager,
+      playerCards = List(Villager, Werewolf, Robber, Tanner, Copycat),
+    )
+    val (finalBoard, history, feedback, _) = playGame(board, List("", "", mockName(0), "", ("middle", mockName(0) + " " + mockName(1))))
+
+    finalBoard(TablePosition.Left).role should be (Villager)
+    finalBoard(TablePosition.Middle).role should be (Troublemaker)
+    finalBoard(TablePosition.Right).role should be (Villager)
+    finalBoard(id(0)).role should be (Werewolf)
+    finalBoard(id(1)).role should be (Robber)
+    finalBoard(id(2)).role should be (Villager)
+    finalBoard(id(3)).role should be (Tanner)
+    finalBoard(id(4)).role should be (Copycat)
+
+    feedback(id(0)) should be (FeedbackMessage.none)
+    feedback(id(2)).mkString should include (mockName(0))
+    feedback(id(2)).mkString should include regex ("(?i)villager")
+    feedback(id(3)) should be (FeedbackMessage.none)
+    feedback(id(4)).mkString should include (mockName(0))
+    feedback(id(4)).mkString should include (mockName(1))
+    feedback(id(4)).mkString should not include regex ("(?i)werewolf|robber|tanner|villager")
+
+  }
+
 }
